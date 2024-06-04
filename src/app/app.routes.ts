bdc80cd54fa19c61af026332d48812eb5d8c77e4
AndresFelipeO <ocampoandres398@gmail.com>
2024-06-03 23:38:51 -0500

    Add: Respuesta service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import path from 'path';
 import { MenuAppComponent } from './feactures/menu-app/menu-app.component';
 import { GestionarDocenteComponent } from './feactures/gestionar-docente/gestionar-docente.component';
 import { GestionarCuestionarioComponent } from './feactures/gestionar-cuestionario/gestionar-cuestionario.component';
+import { GestionarRespuestaComponent } from './feactures/gestionar-respuesta/gestionar-respuesta.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/menu', pathMatch: 'full'},
     {path:'menu', component:MenuAppComponent},
     {path:'gestionarDocente',component:GestionarDocenteComponent},
-    {path:'gestionarCuestionario',component:GestionarCuestionarioComponent}
+    {path:'gestionarCuestionario',component:GestionarCuestionarioComponent},
+    {path:'gestionarRespuesta',component:GestionarRespuestaComponent}
 ];
 
 
